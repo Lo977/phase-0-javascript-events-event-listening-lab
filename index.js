4cc4b437921f2493a66a676d885e8b7089e648ba
Buddha Lo <lo_977@yahoo.com>
2024-01-23 23:40:47 -0800

    add eventlistner

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 function addingEventListener() {
+    const button = document.getElementById("button");
+     function buttonClicked() {
+        alert("I was clicked");
+    }
+    button.addEventListener("CLICK", buttonClicked);
 }
+addingEventListener();
